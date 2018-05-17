5bed1f46963403b0ac6b3d3ef36c8643e1b85256
Mickael Despesse <spocky12@gmail.com>
2018-05-17 17:49:32 +0200

    code cleanup

--- library/src/main/java/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
+++ library/src/main/java/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
@@ -231,14 +231,14 @@ public class SlidingUpPanelLayout extends ViewGroup {
          * @param panel       The child view that was moved
          * @param slideOffset The new offset of this sliding pane within its range, from 0-1
          */
-        public void onPanelSlide(View panel, float slideOffset);
+        void onPanelSlide(View panel, float slideOffset);
 
         /**
          * Called when a sliding panel state changes
          *
          * @param panel The child view that was slid to an collapsed position
          */
-        public void onPanelStateChanged(View panel, PanelState previousState, PanelState newState);
+        void onPanelStateChanged(View panel, PanelState previousState, PanelState newState);
     }
 
     /**
@@ -1084,7 +1084,7 @@ public class SlidingUpPanelLayout extends ViewGroup {
                 }
             }
         }
-        return ViewCompat.canScrollVertically(view, direction);
+        return view.canScrollVertically(direction);
     }
 
     private boolean isViewUnder(View view, int x, int y) {
